a9fad6e27968fdfecbadfff47c7a0e10bd84c292
ruslangalyaviev <ruslan@ipcom.su>
2018-06-22 09:09:35 +0300

    v2.0

--- termostat.py
+++ termostat.py
@@ -3,23 +3,24 @@ from mqtt import *
 import time
 import json
 
-conf = configparser.RawConfigParser()
-conf.read("termostat.conf")
-
 def termostat():
     publish_message_status()
     time.sleep(1)
     last_line = json.load(open("dump.json"))
     last_line = last_line["StatusSNS"]["DS18B20"]["Temperature"]
     print("Прошло 60 сек")
+    conf = configparser.RawConfigParser()
+    conf.read("termostat.conf")
     conf.set("termostat", "tempinroom", last_line)
     with open("termostat.conf", "w") as config:
         conf.write(config)
     if conf.get('termostat', 'status') == 'on':
         if conf.getfloat('termostat', 'tempinroom') <= conf.getfloat('termostat', 'on'):
             publish_message_on()
+            print('Включено')
         if conf.getfloat('termostat', 'tempinroom') >= conf.getfloat('termostat', 'off'):
             publish_message_off()
+            print('Выключено')
 
 while True:
     termostat()
